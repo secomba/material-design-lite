87dd619ebc8b70eb4500f9229d26ffcb420f8a0e
Tommy Leunen <tommy.leunen+github@gmail.com>
2016-03-21 16:28:22 -0400

    1.1.3

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -28,6 +28,7 @@
    */
   var MaterialLayout = function MaterialLayout(element) {
     this.element_ = element;
+    this.innerContainer_ = element.querySelector('.' + this.CssClasses_.INNER_CONTAINER);
 
     // Initialize instance.
     this.init();
@@ -84,7 +85,7 @@
    * @private
    */
   MaterialLayout.prototype.CssClasses_ = {
-    CONTAINER: 'mdl-layout__container',
+    INNER_CONTAINER: 'mdl-layout__inner-container',
     HEADER: 'mdl-layout__header',
     DRAWER: 'mdl-layout__drawer',
     CONTENT: 'mdl-layout__content',
@@ -260,7 +261,7 @@
   * @public
   */
   MaterialLayout.prototype.toggleDrawer = function() {
-    var drawerButton = this.element_.querySelector('.' + this.CssClasses_.DRAWER_BTN);
+    var drawerButton = this.innerContainer_.querySelector('.' + this.CssClasses_.DRAWER_BTN);
     this.drawer_.classList.toggle(this.CssClasses_.IS_DRAWER_OPEN);
     this.obfuscator_.classList.toggle(this.CssClasses_.IS_DRAWER_OPEN);
 
@@ -281,20 +282,13 @@
    */
   MaterialLayout.prototype.init = function() {
     if (this.element_) {
-      var container = document.createElement('div');
-      container.classList.add(this.CssClasses_.CONTAINER);
-
       var focusedElement = this.element_.querySelector(':focus');
-
-      this.element_.parentElement.insertBefore(container, this.element_);
-      this.element_.parentElement.removeChild(this.element_);
-      container.appendChild(this.element_);
 
       if (focusedElement) {
         focusedElement.focus();
       }
 
-      var directChildren = this.element_.childNodes;
+      var directChildren = this.innerContainer_.childNodes;
       var numChildren = directChildren.length;
       for (var c = 0; c < numChildren; c++) {
         var child = directChildren[c];
@@ -317,9 +311,9 @@
       window.addEventListener('pageshow', function(e) {
         if (e.persisted) { // when page is loaded from back/forward cache
           // trigger repaint to let layout scroll in safari
-          this.element_.style.overflowY = 'hidden';
+          this.innerContainer_.style.overflowY = 'hidden';
           requestAnimationFrame(function() {
-            this.element_.style.overflowY = '';
+            this.innerContainer_.style.overflowY = '';
           }.bind(this));
         }
       }.bind(this), false);
@@ -343,7 +337,7 @@
         } else if (this.header_.classList.contains(
             this.CssClasses_.HEADER_SCROLL)) {
           mode = this.Mode_.SCROLL;
-          container.classList.add(this.CssClasses_.HAS_SCROLLING_HEADER);
+          this.element_.classList.add(this.CssClasses_.HAS_SCROLLING_HEADER);
         }
 
         if (mode === this.Mode_.STANDARD) {
@@ -368,20 +362,16 @@
 
       // Add drawer toggling button to our layout, if we have an openable drawer.
       if (this.drawer_) {
-        var drawerButton = this.element_.querySelector('.' +
-          this.CssClasses_.DRAWER_BTN);
-        if (!drawerButton) {
-          drawerButton = document.createElement('div');
-          drawerButton.setAttribute('aria-expanded', 'false');
-          drawerButton.setAttribute('role', 'button');
-          drawerButton.setAttribute('tabindex', '0');
-          drawerButton.classList.add(this.CssClasses_.DRAWER_BTN);
+        var drawerButton = document.createElement('div');
+        drawerButton.setAttribute('aria-expanded', 'false');
+        drawerButton.setAttribute('role', 'button');
+        drawerButton.setAttribute('tabindex', '0');
+        drawerButton.classList.add(this.CssClasses_.DRAWER_BTN);
 
-          var drawerButtonIcon = document.createElement('i');
-          drawerButtonIcon.classList.add(this.CssClasses_.ICON);
-          drawerButtonIcon.innerHTML = this.Constant_.MENU_ICON;
-          drawerButton.appendChild(drawerButtonIcon);
-        }
+        var drawerButtonIcon = document.createElement('i');
+        drawerButtonIcon.classList.add(this.CssClasses_.ICON);
+        drawerButtonIcon.innerHTML = this.Constant_.MENU_ICON;
+        drawerButton.appendChild(drawerButtonIcon);
 
         if (this.drawer_.classList.contains(this.CssClasses_.ON_LARGE_SCREEN)) {
           //If drawer has ON_LARGE_SCREEN class then add it to the drawer toggle button as well.
@@ -407,12 +397,12 @@
         if (this.element_.classList.contains(this.CssClasses_.FIXED_HEADER)) {
           this.header_.insertBefore(drawerButton, this.header_.firstChild);
         } else {
-          this.element_.insertBefore(drawerButton, this.content_);
+          this.innerContainer_.insertBefore(drawerButton, this.content_);
         }
 
         var obfuscator = document.createElement('div');
         obfuscator.classList.add(this.CssClasses_.OBFUSCATOR);
-        this.element_.appendChild(obfuscator);
+        this.innerContainer_.appendChild(obfuscator);
         obfuscator.addEventListener('click',
             this.drawerToggleHandler_.bind(this));
         this.obfuscator_ = obfuscator;
